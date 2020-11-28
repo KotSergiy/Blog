3345f21eeb3dcab1a9df0b666280bc97dede8f59
KotSergiy <ks77vl@gmail.com>
2020-11-28 20:42:00 +0200

    Add model Contact and post data to server

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def contacts
+  end
+
+  def create_contact
+    render plain: params[:contact].inspect
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'home#index'
+  get '/contacts' => 'home#contacts'
+  post '/contacts' => 'home#create_contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
 end
