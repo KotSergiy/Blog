1d9b23facea2306c69373ed5d36f692c93573b65
KotSergiy <ks77vl@gmail.com>
2020-12-05 23:12:25 +0200

    Delete terms and about controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   get '/' => 'home#index'
-  get '/terms' => 'terms#show'
-  get '/about' => 'about#show'
 
   resource :contacts, only: [:new, :create], path_names: {:new=>''}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
