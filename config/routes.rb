2fa698327bd93b5bbd30a9df5b06e207b1a30484
KotSergiy <ks77vl@gmail.com>
2020-12-13 12:36:32 +0200

    Add comments to article

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   resource :contacts, only: [:new, :create], path_names: {:new=>''}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
