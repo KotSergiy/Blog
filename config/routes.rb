8982c9f570ae3027ec67c93075de931109747522
KotSergiy <ks77vl@gmail.com>
2020-12-14 23:30:39 +0200

    Delete useless comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resource :contacts, only: [:new, :create], path_names: {:new=>''}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles do
-    resources :comments
+    resource :comments, only: [:create]
   end
 end
