91e0cd247401cdf65b180ae9d44d5887789a05c5
KotSergiy <ks77vl@gmail.com>
2021-01-05 22:06:45 +0200

    New test for Article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,12 @@
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :text }
+  end
+
+  describe "associations" do
     it { should have_many :comments }
+  end
 end
