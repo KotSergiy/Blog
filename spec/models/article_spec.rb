5130eeef89ffe4d087edfb79a1f434a83ce61dba
KotSergiy <ks77vl@gmail.com>
2021-01-06 22:28:52 +0200

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe Article, type: :model do
   describe "associations" do
     it { should have_many :comments }
   end
+
+  describe "#subject" do
+    it "returns the article title" do
+      user=create(:user)
+      article=create(:article, title: 'La-la-la', user: user)
+      expect(article.subject).to eq 'La-la-la'
+    end
+  end
 end
