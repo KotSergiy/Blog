08e164b99cb2e99377e8f79d5442290ee20816b0
KotSergiy <ks77vl@gmail.com>
2021-01-07 23:04:02 +0200

    Update accept test create account

--- spec/features/visitor_create_account_spec.rb
+++ spec/features/visitor_create_account_spec.rb
@@ -1,6 +1,11 @@
 require "rails_helper"
 
 feature "Account Creation" do
+  after(:all) do
+    # before/after(:all) is not transactional; see https://www.relishapp.com/rspec/rspec-rails/docs/transactions
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  
   scenario "allows a guest to create account" do
     sign_up
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
