80b2c183fec5d07d5d7400ccb73d6065780a9f93
KotSergiy <ks77vl@gmail.com>
2021-01-07 14:43:44 +0200

    Add tests validate_of_length

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,8 @@
 require "rails_helper"
 
 RSpec.describe Comment, type: :model do
+  describe "validations" do
     it { should belong_to :article }
+    it { should validate_length_of(:body).is_at_most(4000) }
+  end
 end
