78045b0c3f031e9b1399a8b4cb2220fca154455b
KotSergiy <ks77vl@gmail.com>
2021-01-07 19:54:29 +0200

    Add accept test create contact

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -5,4 +5,14 @@ feature "Contact Creation" do
     visit ('/contacts')
     expect(page).to have_content I18n.t('contacts.contact_us')
   end
+
+  scenario "allows a guest to create contact" do
+    visit ('/contacts')
+
+    fill_in :contact_email, :with => 'user@example.com'
+    fill_in :contact_message, :with => 'hello!!!'
+    click_button 'Отправить'
+
+    expect(page).to have_content 'Отправлено!'
+  end
 end
